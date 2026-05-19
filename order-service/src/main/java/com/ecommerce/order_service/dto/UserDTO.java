6649cf911213c6321e8ac1908c42ac7f7c0c3dbc
Yogeshwaran <yogim3110@gmail.com>
2026-05-19 11:00:13 +0530

    Api Gateway Service added

---
+++ order-service/src/main/java/com/ecommerce/order_service/dto/UserDTO.java
@@ -0,0 +1,7 @@
+package com.ecommerce.order_service.dto;
+
+public class UserDTO {
+    private Long id;
+    private String name;
+    private int age;
+}
--- order-service/src/main/java/com/ecommerce/order_service/feign/UserClient.java
+++ order-service/src/main/java/com/ecommerce/order_service/feign/UserClient.java
@@ -1,5 +1,6 @@
 package com.ecommerce.order_service.feign;
 
+import com.ecommerce.order_service.dto.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,5 +9,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface UserClient {
 
     @GetMapping("/users/{id}")
-    Object getUser(@PathVariable("id") Long id);
+    UserDTO getUser(@PathVariable("id") Long id);
 }
